83a17947fc20e5a7f73511c0df193b83f07011a6
Chinmoy Das <chinmaydas3736@gmail.com>
2026-06-05 10:57:46 +0530

    Time: 0 ms (100%), Space: 12.6 MB (25.44%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+ void subsequence (vector<int>&arr,int index,int n,vector<vector<int> >&ans,vector<int>&temp)
+ {
+    if(index==n) {
+     ans.push_back(temp);   
+    return;
+    }
+    
+    // not including
+    subsequence(arr,index+1,n,ans,temp);
+    // if included
+    temp.push_back(arr[index]);
+    subsequence(arr,index+1,n,ans,temp);
+    temp.pop_back();
+ }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int> >ans;
+        vector<int>temp;
+        subsequence(nums,0,nums.size(),ans,temp);
+        return ans;
+    }
+};
