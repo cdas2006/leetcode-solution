205cf0e49e6618d0fe5f8a0ea1cf06e0e7b31e0d
Chinmoy Das <chinmaydas3736@gmail.com>
2026-06-19 17:16:36 +0530

    Time: 7 ms (38.87%), Space: 7.9 MB (79.64%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+
+int winner(vector<bool>& person,int n, int index, int person_left,int k)
+{
+    if(person_left==1)
+    {
+        for(int i=0;i<n;i++)
+        {
+            if(person[i]==0)
+            {
+                return i;
+            }
+        }
+    }
+
+    // find the position for kill
+
+    int kill=(k-1)%person_left;
+
+    while(kill--)
+    {
+        index=(index+1)%n;
+
+        while(person[index]==1)
+        {
+            index=(index+1)%n; // skip the killed person 
+        }
+
+    }
+
+        person[index]=1;
+
+        // find next alive person
+          
+           while(person[index]==1)
+        {
+            index=(index+1)%n; // skip the killed person 
+        }
+
+       return winner(person,n,index,person_left-1,k); 
+
+    
+}
+    int findTheWinner(int n, int k) {
+        
+        vector<bool>person(n,0);
+        return winner(person,n,0,n,k)+1;
+    }
+};
