f202d864f60e0192adebea53d4ae936f28349a1f
Chinmoy Das <chinmaydas3736@gmail.com>
2026-07-20 20:04:16 +0530

    Time: 0 ms (100%), Space: 63.7 MB (10.9%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        
+        int start=0,end=arr.size()-1,mid;
+        while (start<=end){
+            mid=end+(start-end)/2;
+            //peak element
+            if (arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])
+                return mid;
+            //right side move
+           else if (arr[mid]<arr[mid+1])
+            {
+                start=mid+1;
+            }
+            //left side move
+            else if (arr[mid]>arr[mid+1]) {
+                end=mid-1;
+            }
+        }
+
+        return -1;
+
+    }
+};
